8f7b97c098e831cc8251ca355d9090b3b435246d
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2018-08-31 16:51:56 +0200

    weiter testen

---
+++ Afg_30_1/Afg_30_1.rs
@@ -0,0 +1,20 @@
+/*
+Aufgabenstellung:
+Array mit 100 Float Zahlen erstellen.
+Arrayzugriff auf 3 Arten:
+-Indizes x[i]
+-Berechnete Zeiger *(x+i)
+-Inkrementierter Zeiger x++
+
+Berechnung von:
+-Maximum + Index
+-Minimum + Index
+-Index des Elementes mit der Größten Abweichung.
+-Standartabweichung
+
+*/
+
+fn main() {
+
+
+}
