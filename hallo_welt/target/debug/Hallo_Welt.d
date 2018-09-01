1f59981f50fb03550818aac490cfc46b5f835dd3
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2018-09-01 10:45:11 +0200

    test

--- hallo_welt/target/debug/Hallo_Welt.d
+++
@@ -1,1 +0,0 @@
-D:\Bibliotheken\Workbench\VS17\Projekte\ Rust\Rust_lernen\Hallo_Welt\target\debug\Hallo_Welt.pdb: D:\Bibliotheken\Workbench\VS17\Projekte\ Rust\Rust_lernen\Hallo_Welt\src\main.rs
--- hallo_welt/target/debug/deps/Hallo_Welt-fe3385f739ae6da5.d
+++
@@ -1,5 +0,0 @@
-D:\Bibliotheken\Workbench\VS17\Projekte Rust\Rust_lernen\Hallo_Welt\target\debug\deps\Hallo_Welt-fe3385f739ae6da5.exe: src\main.rs
-
-D:\Bibliotheken\Workbench\VS17\Projekte Rust\Rust_lernen\Hallo_Welt\target\debug\deps\Hallo_Welt-fe3385f739ae6da5.d: src\main.rs
-
-src\main.rs:
