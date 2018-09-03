c4e745a9d7c41d381c9153437765c4315c0b9c2e
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2018-09-03 12:10:33 +0200

    Afg_70_2  created

---
+++ afg_70_2/src/main.rs
@@ -0,0 +1,12 @@
+/*
+Entwickeln sie für den komplexen Kehrwert 1/z ein weiteres Unterprogramm.
+Nutzen Sie dieses und die schon bekannten Unterprogramme um den komplexen
+Gesamtwiderstand von Wechselstromschaltungen zu Berechnnen.
+
+Erstellen sie eine Tabelle die die Wechselstromwiderstände in Abhängigkeit Frequenz f darstellt.
+f: [10khz,680khz]
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
