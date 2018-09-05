d3d4af89d9ae4910c14eba183b3c0a4da2c2c8a2
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2018-09-05 09:38:16 +0200

    Update afg_30_1

--- afg_30_1/src/bin/main.rs
+++ afg_30_1/src/bin/main.rs
@@ -15,27 +15,30 @@ use rand::prelude::*;               //Für den Zufallszahlengenerator
 use std::time::Instant;             //Für Zeitmessungen
 use std::f32;                       //Für minimal und Maximalwerte von Gleitkommazahlen
 
-fn get_max(array:&[f32]){    
+fn get_max(array:&[f32]) -> (f32,bool) {    
     let mut rand_max = f32::MIN;
     for i in 0..array.len(){
         if rand_max < array[i] {rand_max = array[i];}        
-    }    
+    }
+    return (rand_max,true);
 }
 
-fn get_min(array:&[f32]){
+fn get_min(array:&[f32]) -> (f32,bool) {
     let mut rand_min = f32::MAX;
     for i in 0..array.len(){
         if rand_min > array[i] {rand_min = array[i];}
     }
+    return (rand_min,true);
 }
 
-fn get_max_dev(array:&[f32]){
+fn get_max_dev(array:&[f32]) -> (f32,usize,bool) {
     let mut average:f32 = 0.0;
     let mut max_dev:f32 = f32::MIN;
     let mut index_dev:usize = 0;
     for i in 0..array.len(){
         average += array[i];
     }
+    average /= array.len() as f32;
     for i in 0..array.len(){
         let dev = array[i] - average;   //Berechnung der aktuellen differenz
         if dev.abs() > max_dev {        //Wenn die Abweichung größer als die bisherige maximale Abweichung war,
@@ -43,6 +46,7 @@ fn get_max_dev(array:&[f32]){
             index_dev = i;              //aktualisiere den Index
         }
     }
+    return (max_dev,index_dev,true);
 }
 
 fn main() {
@@ -88,25 +92,41 @@ fn main() {
     println!("Maximale Abweichung: {}", max_dev);
     println!("Index: {} \n", index_dev);
 
-    //Test mittels Future Crate
-    let mut array : [f32;1000] = [0.0;1000];//Arraydeklaration mit 1000 Feldern [Datentyp;Anzahl] initialisiert mit 0.0
-                                            //Alternative: Listendeklaration [0,1,2,3,4,5,7...] (wäre nur etwas anstrengend) 
-                                            //TODO herausfinden ob man das Array direkt mit Zufallszahlen initialisieren kann
-    let pool = ThreadPool::new(4);
-    let test = 1.0;
-    pool.execute(move |test|{
-        println!("Test1: {}",test);
-    });
-     /*
-    let mut rand_max = poll_fn(read_line());  
-    let mut rand_min: f32 = f32::MAX;
-    let mut average:  f32 = 0.0;
-    let mut max_dev: f32  = f32::MIN;
-    let mut index_dev: usize=0;
+    //Test mittels Threadpool:
 
-    println!("{:?}",rand_max);
-    */
+    let mut array : [f32;1000] = [0.0;1000];
+    let mut rand_max: (f32,bool) = (f32::MIN,false);
+    let mut rand_min: (f32,bool) = (f32::MAX,false);
+    let mut average:  (f32,bool) = (0.0,false);
+    let mut max_dev: (f32,usize,bool) = (0.0,0,false);
 
     let now = Instant::now();
+
+    for i in 0..array.len(){
+        array[i] = random::<f32>();         //speichere eine Zufallszahl an die Stelle des Arrays
+    }
+
+    println!("Zeit vergangen - nach füllen des Arrays: {:?}",now.elapsed() );
+    
+
+    let pool = ThreadPool::new(4);
+
+    pool.execute(move ||{
+        rand_max = get_max(&array);        
+    });
+    pool.execute(move ||{
+        rand_min = get_min(&array);        
+    });
+    pool.execute(move ||{
+        max_dev = get_max_dev(&array);        
+    });
+    /*while (rand_max.1 == false || rand_min.1 == false || max_dev.2 == false) {
+        println!("läuft!")
+    }*/
+    println!("Zeit vergangen - nach Auswertung: {:?} \n",now.elapsed());
+        
+    println!("Maximum: {}",rand_max.0);
+    println!("Minimum: {}",rand_min.0);
+    println!("Maximale Abweichung: {} bei : {}",max_dev.0,max_dev.1);
 
 }
--- afg_30_1/src/lib.rs
+++ afg_30_1/src/lib.rs
@@ -3,98 +3,84 @@ use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 
-enum Message {
+enum Message {  //Nachrichten-Enum
     NewJob(Job),
     Terminate,
 }
 
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
+struct Worker {                             //Workerstruct
+    id: usize,                              //ID des Workers
+    thread: Option<thread::JoinHandle<()>>, //Thread des Workers
 }
 
-pub struct ThreadPool{
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Message>,
+pub struct ThreadPool{              //Thread Pool
+    workers: Vec<Worker>,           //Worker innerhalb des Pools
+    sender: mpsc::Sender<Message>,  //Sender um Nachrichten zum Worker zu senden
 }
 
 trait FnBox {
-    fn call_box(self: Box<Self>);
+    fn call_box(self: Box<Self>);   //Hat irgendwas mit dem Ausführen der Funktion zu tun
 }
 
 impl<F: FnOnce()> FnBox for F {
-    fn call_box(self: Box<F>) {
+    fn call_box(self: Box<F>) {     //Hat etwas mit dem ausführen der Funktion zu tun
         (*self)()
     }
 }
 
-type Job = Box<FnBox + Send + 'static>;
+type Job = Box<FnBox + Send + 'static>; //Job Datentyp um Funktionen auszuführen
 
 impl ThreadPool {
-   pub fn new(size: usize) -> ThreadPool {//Erstellt den Threadpool
-        assert!(size > 0);
-        let mut workers = Vec::with_capacity(size);
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-
+   pub fn new(size: usize) -> ThreadPool {                          //Erstellt den Threadpool
+        assert!(size > 0);                                          //Panic wenn die Größe nicht größer als 0 ist
+        let mut workers = Vec::with_capacity(size);                 //Erstelle Workerthreads 
+        let (sender, receiver) = mpsc::channel();                   //Erstelle den Channel über den die Jobs an die Worker verteilt werden und die Threads terminiert werden können
+        let receiver = Arc::new(Mutex::new(receiver));              //Schütze den Receiver mit einem Arc (notwendig damit nicht mehrere Threads auf den Receiver zugreifen)
         for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));   //Erstelle Worker
         }
-        ThreadPool {
+        ThreadPool {                                                //Erstelle den Threadpool mit den Workern und einem Sender
             workers,
             sender,
         }
     }
 
-    pub fn execute<F>(&self, f: F)          //führt eine Funktion aus
-        where F: FnOnce() + Send + 'static {
-            let job = Box::new(f);
-            self.sender.send(Message::NewJob(job)).unwrap();
+    pub fn execute<F>(&self, f: F)                              //Führt eine Funktion aus
+        where F: FnOnce() + Send + 'static {                    
+            let job = Box::new(f);                              //Alloziiere Speicher für die Funktion
+            self.sender.send(Message::NewJob(job)).unwrap();    //Sende die Funktion zum Worker
     }
 
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
-        let thread = thread::spawn(move ||{
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker { //Erstellt einen Worker
+        let thread = thread::spawn(move ||{ //Erstellt einen Thread
             loop {
-                let message = receiver.lock().unwrap().recv().unwrap();
-
-                match message {
-                    Message::NewJob(job) => {
-                        println!("Worker {} got a job; executing.", id);
-
-                        job.call_box();
+                let message = receiver.lock().unwrap().recv().unwrap(); //Versuche eine Nachricht zu empfangen
+                match message {                                         //Werte die Nachricht aus
+                    Message::NewJob(job) => {                           //Wenn die Nachricht "neuer Job" lautet:
+                        job.call_box();                                 //Rufe die Funktion die der Job enthält
                     },
-                    Message::Terminate => {
-                        println!("Worker {} was told to terminate.", id);
-
-                        break;
+                    Message::Terminate => {                             //Wenn die Nachricht "Terminate" lautet
+                        break;                                          //beende den Thread
                     },
                 }
             }
         });
-
-        Worker {
+        Worker {    //Erstelle den Worker
             id,
             thread: Some(thread),
         }
     }
 }
 
-
 impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        println!("Sending terminate message to all workers.");
-
+    fn drop(&mut self) { 
         for _ in &mut self.workers {
             self.sender.send(Message::Terminate).unwrap();
         }
-
-        println!("Shutting down all workers.");
-
         for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
 
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
