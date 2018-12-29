63ce509fd29f0e26e29ca9ef3ba0908ec9cfff79
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2018-12-29 14:29:50 +0100

    Update main.rs

--- afg_30_1/src/main.rs
+++ afg_30_1/src/main.rs
@@ -14,6 +14,8 @@ use rayon::prelude::*;
 use rand::prelude::*;               //Für den Zufallszahlengenerator
 use std::time::Instant;             //Für Zeitmessungen
 use std::f32;                       //Für minimal und Maximalwerte von Gleitkommazahlen
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap(); //erstelle den Threadpool
@@ -32,6 +34,8 @@ fn main() {
         array[i] = random::<f32>();         //speichere eine Zufallszahl an die Stelle des Arrays
     }
 
+
+    /*________________________________________*/
     //Versuch ohne Parallelbearbeitung
     let now = Instant::now();
     println!("Zeit vergangen - Start normal: {:?}",now.elapsed() );
@@ -41,8 +45,7 @@ fn main() {
         if rand_min > array[i] {rand_min = array[i];}
         average += array[i];
     }
-    average /= 1000.0; //sollte gegen 0.5 gehen
-
+    average /= array.len() as f32; //sollte gegen 0.5 gehen
     //Berechnung der maximalen Abweichung
     for i in 0..array.len(){
         let dev = array[i] - average;   //Berechnung der aktuellen differenz
@@ -58,20 +61,67 @@ fn main() {
     println!("Maximale Abweichung: {}", max_dev);
     println!("Index: {} \n", index_dev);
 
-    //Versuch mit Parallelbearbeitung:
+
+    /*________________________________________*/
+    //Versuch mit Parallelbearbeitung - Rayon:
 
     let now = Instant::now(); //Beginne Zeitmessung    
-    println!("Zeit vergangen - Start parallel: {:?}",now.elapsed() );
+    println!("Zeit vergangen - Start Rayon: {:?}",now.elapsed() );
 
-    let par_max = array.par_iter().map(|b|*b as f32).reduce(||f32::MIN,|a, b| a.max(b)) ; //Suche das Maximum
-    let par_min = array.par_iter().map(|b|*b as f32).reduce(||f32::MAX,|a, b| a.min(b)) ; //Suche das Minimum    
-    let mut par_avg: f32 = array.par_iter().sum(); //addiere werte und berechne den Durchschnitt
-    par_avg /= array.len() as f32;
-    let par_max_dev = 0;
+    let ray_max = array.par_iter().map(|b|*b as f32).reduce(||f32::MIN,|a, b| a.max(b)) ; //Suche das Maximum
+    let ray_min = array.par_iter().map(|b|*b as f32).reduce(||f32::MAX,|a, b| a.min(b)) ; //Suche das Minimum    
+    let mut ray_avg: f32 = array.par_iter().sum(); //addiere Werte und berechne den Durchschnitt
+    ray_avg /= array.len() as f32;
+    let ray_max_dev = 0.0;
 
-    println!("Zeit vergangen - Ende parallel: {:?}",now.elapsed() );
-    println!("Maximum im Array: {}", par_max );
-    println!("Minimum im Array: {}", par_min );
-    println!("Durchschnitt: {}", par_avg );
+    println!("Zeit vergangen - Ende Rayon: {:?} \n",now.elapsed());
+    println!("Maximum im Array: {}", ray_max);
+    println!("Minimum im Array: {}", ray_min);
+    println!("Durchschnitt: {}", ray_avg );
+    println!("Maximale Abweichung: {} \n", ray_max_dev);
 
+    /*________________________________________*/
+    //Versuch mit Parallelbearbeitung - Threads:
+    let th_min = Arc::new(Mutex::new(f32::MAX));
+    let th_max = Arc::new(Mutex::new(f32::MIN));
+    let th_avg = Arc::new(Mutex::new(0.0));
+    let th_max_dev = Arc::new(Mutex::new(0.0));
+
+    let now = Instant::now(); //Beginne Zeitmessung    
+    println!("Zeit vergangen - Start Threads: {:?}",now.elapsed() );
+
+    let array1 = array.clone();
+    let t1 = thread::spawn(||{
+        let mut min = th_min.clone().lock().unwrap();
+        *min = array1.iter().fold(f32::MAX, |a, &b| a.min(b));
+    });
+
+    let array2 = array.clone();
+    let t2 = thread::spawn(||{
+        let mut max = th_max.clone().lock().unwrap();
+        *max = array2.iter().fold(f32::MIN, |a, &b| a.max(b));
+    });
+
+    let array3 = array.clone();
+    let t3 = thread::spawn(||{
+        let mut avg = th_avg.clone().lock().unwrap();
+        *avg = array3.iter().sum();
+        *avg /= array.len() as f32;
+    });
+    let now = Instant::now(); //Beginne Zeitmessung
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    let th_max_v = th_max.lock().unwrap();
+    let th_min_v = th_min.lock().unwrap();
+    let th_avg_v = th_avg.lock().unwrap();
+
+
+    println!("Zeit vergangen - Ende Threads: {:?} \n",now.elapsed());
+    println!("Maximum im Array: {}", th_max_v);
+    println!("Minimum im Array: {}", th_min_v);
+    println!("Durchschnitt: {}", th_avg_v );
+    
 }
