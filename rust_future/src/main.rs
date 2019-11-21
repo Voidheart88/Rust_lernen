ecac02b3ded92372e647c899f232e3813098eed5
Voidheart88 <39334902+Voidheart88@users.noreply.github.com>
2019-11-21 08:53:40 +0100

    update Rust fut

--- rust_future/src/main.rs
+++ rust_future/src/main.rs
@@ -1,3 +1,12 @@
+use async_std::task;
+
 fn main() {
-    println!("Hello, world!");
+    let child1 = task::spawn(async {
+        println!("Hello, world1!");
+    });
+    let child2 = task::spawn(async {
+        println!("Hello, world2!");
+    });
+    task::block_on(child2);
+    task::block_on(child1);
 }
